7aa181f966e59666a81d378eb100582f826d97f1
camara94 <ldamaro98@gmail.com>
2020-03-10 10:58:38 +0100

    Création du modele "classe Produit"

---
+++ mappingObjectRelational/src/main/java/com/camaratek/model/Produit.java
@@ -0,0 +1,56 @@
+package com.camaratek.model;
+
+import java.io.Serializable;
+
+public class Produit implements Serializable {
+	
+	private Long id;
+	private String designation;
+	private double prix;
+	private int quantite;
+	
+	public Produit() {}
+
+	public Produit(Long id, String designation, double prix, int quantite) {
+		super();
+		this.id = id;
+		this.designation = designation;
+		this.prix = prix;
+		this.quantite = quantite;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDesignation() {
+		return designation;
+	}
+
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+
+	public double getPrix() {
+		return prix;
+	}
+
+	public void setPrix(double prix) {
+		this.prix = prix;
+	}
+
+	public int getQuantite() {
+		return quantite;
+	}
+
+	public void setQuantite(int quantite) {
+		this.quantite = quantite;
+	}
+	
+	
+	
+}
